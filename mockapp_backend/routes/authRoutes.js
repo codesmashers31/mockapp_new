8a6d0c35eb46a90baa17278b328cced20808f375
abdulrashik <abdulrashik678@gmail.com>
2025-08-30 19:49:52 +0530

    login updated

--- mockapp_backend/routes/authRoutes.js
+++ mockapp_backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { sendOtp, verifyOtp } from "../controllers/authController.js";
+import { sendOtp, verifyOtp, registerUser, loginUser, authenticateToken, getProfile } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post('/send-otp',sendOtp);
-router.post('/verify-otp',verifyOtp);
-
+router.post('/send-otp', sendOtp);
+router.post('/verify-otp', verifyOtp);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', authenticateToken, getProfile);
 
 export default router;
